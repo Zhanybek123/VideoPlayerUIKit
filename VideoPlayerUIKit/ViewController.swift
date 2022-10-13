1d727fc22c59ec715c0323c483b495e06b6b555b
zhany <zhanybeksalgarin@gmail.com>
2022-10-13 12:38:38 -0400

    Add Video

--- VideoPlayerUIKit/ViewController.swift
+++ VideoPlayerUIKit/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
 
+    private func playVideo() {
+        guard let path = Bundle.main.path(forResource: "performance", ofType: "mp4") else {
+            debugPrint("Something went wrong with provided mp4")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
+    
 }
 
